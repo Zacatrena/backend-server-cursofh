c3d3eed6df6f4065655a6e37652ef57f0a85ed35
Jose Luis Martin <joseluis.martin@cast-info.es>
2018-08-30 17:27:05 +0200

    Primer commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+// Inicializar variables
+var app = express();
+
+
+// Conexión a la BD
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+});
+
+
+// Rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    });
+});
+
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express Server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
